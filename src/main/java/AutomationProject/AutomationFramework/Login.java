08819f11442d677438e078193fa269e45977b676
Rahul <New Kamal@DESKTOP-ED1NJ38>
2024-08-06 22:35:01 +0530

    added sendkeys

--- src/main/java/AutomationProject/AutomationFramework/Login.java
+++ src/main/java/AutomationProject/AutomationFramework/Login.java
@@ -18,7 +18,7 @@ public class Login {
 		driver.findElement(By.xpath("//span[text()='My Account']")).click();
 		driver.findElement(By.xpath("//a[text()='Login']")).click();
 		driver.findElement(By.xpath("//input[@name='email']")).sendKeys("gaikwadrahul821@gmail.com");
-		driver.findElement(By.xpath("//input[@type='password']")).sendKeys("Buddham@821");
+		driver.findElement(By.xpath("//input[@type='password']")).sendKeys("Buddham@0821");
 		//driver.findElement(By.xpath("//button[@type='submit']")).click();
 		driver.findElement(By.xpath("//button[text()='Login']")).click();
 		String ExpectedUrl= driver.getCurrentUrl();
